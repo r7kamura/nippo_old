3de7383328c09dab8c29b3331d1ac5cc394a69f6
Ryo Nakamura <r7kamura@gmail.com>
2022-05-06 10:14:16 +0900

    Remove date from comments, not necessary for nippo

--- pages/articles/[issueNumber].tsx
+++ pages/articles/[issueNumber].tsx
@@ -35,9 +35,6 @@ const ShowArticle: NextPage<Props> = ({ issue, issueComments }) => {
       {issueComments.map((issueComment) => (
         <article key={issueComment.id}>
           <section>
-            <header>
-              <Time dateTime={issueComment.created_at} />
-            </header>
             <div
               dangerouslySetInnerHTML={{ __html: issueComment.bodyHTML }}
             ></div>
